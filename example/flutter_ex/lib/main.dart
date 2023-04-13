b411e15c389fd515eba2561d90a0e374282329a6
Igor K <igor.k@letsmake.com>
2023-04-13 03:18:15 +0300

    update flutter example, add local assets sample

--- example/flutter_ex/lib/main.dart
+++ example/flutter_ex/lib/main.dart
@@ -4,11 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:epub/epub.dart' as epub;
 import 'package:image/image.dart' as image;
+import 'package:flutter/services.dart' show rootBundle;
 
-void main() => runApp(EpubWidget());
+void main() => runApp(const EpubWidget());
 
 class EpubWidget extends StatefulWidget {
-  EpubWidget({Key? key}) : super(key: key);
+  const EpubWidget({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => EpubState();
@@ -25,6 +26,12 @@ class EpubState extends State<EpubWidget> {
     });
   }
 
+  void loadLocalBookButton() {
+    setState(() {
+      book = loadLocalBook();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,66 +41,70 @@ class EpubState extends State<EpubWidget> {
         child: Container(
           padding: const EdgeInsets.all(30.0),
           color: Colors.white,
-          child: Container(
-            child: Center(
-              child: ListView(
-                children: [
-                  Padding(padding: EdgeInsets.only(top: 70.0)),
-                  Text('Epub Inspector', style: TextStyle(fontSize: 25.0)),
-                  Padding(padding: EdgeInsets.only(top: 50.0)),
-                  Text(
-                    'Enter the Url of an Epub to view some of it\'s metadata.',
-                    style: TextStyle(fontSize: 18.0),
-                    textAlign: TextAlign.center,
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 20.0)),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Enter Url',
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(),
-                      ),
+          child: Center(
+            child: ListView(
+              children: [
+                const Padding(padding: EdgeInsets.only(top: 70.0)),
+                const Text('Epub Inspector', style: TextStyle(fontSize: 25.0)),
+                const Padding(padding: EdgeInsets.only(top: 50.0)),
+                const Text(
+                  'Enter the Url of an Epub to view some of it\'s metadata.',
+                  style: TextStyle(fontSize: 18.0),
+                  textAlign: TextAlign.center,
+                ),
+                const Padding(padding: EdgeInsets.only(top: 20.0)),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter Url',
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: const BorderSide(),
                     ),
-                    validator: (val) {
-                      if (val?.length == 0) {
-                        return 'Url cannot be empty';
-                      } else {
-                        return null;
+                  ),
+                  validator: (val) {
+                    if (val?.isEmpty == true) {
+                      return 'Url cannot be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  controller: _urlController,
+                  keyboardType: TextInputType.url,
+                  autocorrect: false,
+                  style: const TextStyle(fontFamily: 'Poppins'),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 20.0)),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(8.0)),
+                  onPressed: fetchBookButton,
+                  child: const Text('Inspect Book'),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 25.0)),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(8.0)),
+                  onPressed: loadLocalBookButton,
+                  child: const Text('Inspect Local Book'),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 25.0)),
+                Center(
+                  child: FutureBuilder<epub.EpubBookRef>(
+                    future: book,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Material(color: Colors.white, child: buildEpubWidget(snapshot.data!));
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
                       }
-                    },
-                    controller: _urlController,
-                    keyboardType: TextInputType.url,
-                    autocorrect: false,
-                    style: TextStyle(fontFamily: 'Poppins'),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 20.0)),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(8.0)),
-                    onPressed: fetchBookButton,
-                    child: Text('Inspect Book'),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 25.0)),
-                  Center(
-                    child: FutureBuilder<epub.EpubBookRef>(
-                      future: book,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          return Material(color: Colors.white, child: buildEpubWidget(snapshot.data!));
-                        } else if (snapshot.hasError) {
-                          return Text("${snapshot.error}");
-                        }
-                        // By default, show a loading spinner
-                        // return CircularProgressIndicator();
+                      // By default, show a loading spinner
+                      // return CircularProgressIndicator();
 
-                        // By default, show just empty.
-                        return Container();
-                      },
-                    ),
+                      // By default, show just empty.
+                      return Container();
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
@@ -102,42 +113,40 @@ class EpubState extends State<EpubWidget> {
   }
 
   Widget buildEpubWidget(epub.EpubBookRef book) {
-    var chapters = book.getChapters();
-    var cover = book.readCover();
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Text('Title', style: TextStyle(fontSize: 20.0)),
-          Text(book.title, style: TextStyle(fontSize: 15.0)),
-          Padding(padding: EdgeInsets.only(top: 15.0)),
-          Text('Author', style: TextStyle(fontSize: 20.0)),
-          Text(book.author, style: TextStyle(fontSize: 15.0)),
-          Padding(padding: EdgeInsets.only(top: 15.0)),
-          Column(
-            children: <Widget>[
-              Text('Chapters', style: TextStyle(fontSize: 20.0)),
-              Text(chapters.length.toString(), style: TextStyle(fontSize: 15.0)),
-            ],
-          ),
-          Padding(padding: EdgeInsets.only(top: 15.0)),
-          FutureBuilder<epub.Image?>(
-            future: cover,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  children: <Widget>[
-                    Text('Cover', style: TextStyle(fontSize: 20.0)),
-                    Image.memory(image.encodePng(snapshot.data!) as Uint8List),
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              return Container();
-            },
-          ),
-        ],
-      ),
+    final chapters = book.getChapters();
+    final cover = book.readCover();
+    return Column(
+      children: <Widget>[
+        const Text('Title', style: TextStyle(fontSize: 20.0)),
+        Text(book.title, style: const TextStyle(fontSize: 15.0)),
+        const Padding(padding: EdgeInsets.only(top: 15.0)),
+        const Text('Author', style: TextStyle(fontSize: 20.0)),
+        Text(book.author, style: const TextStyle(fontSize: 15.0)),
+        const Padding(padding: EdgeInsets.only(top: 15.0)),
+        Column(
+          children: <Widget>[
+            const Text('Chapters', style: TextStyle(fontSize: 20.0)),
+            Text(chapters.length.toString(), style: const TextStyle(fontSize: 15.0)),
+          ],
+        ),
+        const Padding(padding: EdgeInsets.only(top: 15.0)),
+        FutureBuilder<epub.Image?>(
+          future: cover,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: <Widget>[
+                  const Text('Cover', style: TextStyle(fontSize: 20.0)),
+                  Image.memory(image.encodePng(snapshot.data!) as Uint8List),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return Container();
+          },
+        ),
+      ],
     );
   }
 
@@ -156,5 +165,11 @@ class EpubState extends State<EpubWidget> {
       // If that response was not OK, throw an error.
       throw Exception('Failed to load epub');
     }
+  }
+
+  Future<epub.EpubBookRef> loadLocalBook() async {
+    ByteData bytes = await rootBundle.load('assets/x.epub');
+    final source = bytes.buffer.asUint8List();
+    return epub.EpubReader.openBook(source);
   }
 }
